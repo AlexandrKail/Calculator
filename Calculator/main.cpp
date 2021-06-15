9e546364c793e6b00041bd896758a5647d4a4d99
Alexandr <kajlaleksandr1986@gmail.com>
2021-06-15 17:51:54 +0700

    Оптимизировал

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 
+//#Пеоеводит символы в числа#
+//#Принимает: массив данных, текущий указатель на последний символ пер. числа, кол. символов в переводимом числе# 
 int charToInt(char* input,int i,int numb)
 {
 	int b{};
@@ -11,66 +13,70 @@ int charToInt(char* input,int i,int numb)
 	return b;
 }
 
+//#Получает значения от пользователя и переводет их в символы#
+//#Принимает: массив для данных, размер массива#
 void userInput(char* input, size_t size)
 {
-	using namespace std;
-	{
-		int a{};
+		int val{};
 		char simb{};
 		int i{1};
 		input[0] = '!';
-		cout << "Введите вырожение типа \"a#b#c=\" где # = +,-,*,/: ";
-		do
+		std::cout << "Введите вырожение типа \"a#b#c=\" где \"#\" символы \"+,-,*,/\", а \"a,b,c\" десятичные числа:\n>> ";
+
+		while(simb != '=')
 		{
 
-			cin >> a;
-			i += sprintf_s(input + i,size-i, "%d", a);
-//			cout << "Введите символ(+,-,*,/): ";
-			cin >> simb;
+			std::cin >> val;
+			i += sprintf_s(input + i,size-i, "%d", val);
+			std::cin >> simb;
 			input[i] = simb;
 			i++;
-		} while (simb != '=');
-	}
+		} 
 }
 
-void compArray(char* input,size_t size,int i ,int result,int numb1,int numb2)
+//#Сжатие массива путём записи вычисленных значений#
+//#Принимает: Массив с данными, размер массива, текущая поз. в массиве, результат вырожения,
+//кол. символов лев. числа от ар. знака, кол. символов правого числа#
+void compArray(char* input,size_t size,int i ,int result,int lnumb,int rnumb)
 {
 	int foo{};
-	if (input[i + (numb2+1)] == '=')
+	if (input[i + (rnumb+1)] == '=')
 	{
-		foo=sprintf_s(input + (i-numb1), size - i, "%d", result);
-//		input[i - 1] = intToChar(result);
-		input[i-numb1+foo] = '=';
+		foo=sprintf_s(input + (i-lnumb), size - i, "%d", result);
+		input[i-lnumb+foo] = '=';
 	}
 	else
 	{
 		char buf[256];
 		int j{}, k{i};
 
-		while(input[k + numb2+1] != '=')
+		while(input[k + rnumb+1] != '=')
 		{
-			buf[j] = input[k+numb2+1];
+			buf[j] = input[k+rnumb+1];
 			j++;
 			k++;
 		}
 		buf[j] = '=';
 		j = 0;
-		foo = sprintf_s(input + (i - numb1), size - i, "%d", result);
+		foo = sprintf_s(input + (i - lnumb), size - i, "%d", result);
 		while (buf[j] != '=')
 		{
-			input[i-numb1+foo] = buf[j];
+			input[i-lnumb+foo] = buf[j];
 			j++;
 			i++;
 		}
-		input[i-numb1+foo] = '=';
+		input[i-lnumb+foo] = '=';
 	}
 
 }
 
-int numbSize(char* input, int i,bool flag)
+//#Вычесляет колличество символов числа, до арифмитического знака или после#
+//#Принимает: массив с данными, текущая позиция в массиве, флаг направления  true - 
+//в право(число находящаяся с право от ар. знака), false - влево.#
+int numbSize(char* input, int i,bool direction)
 {
 	int numb{};
-	if (flag != true)
+	if (direction != true)
 	{
 		while (input[i - 1] != '+' && input[i - 1] != '-' && input[i - 1] != '*' && input[i - 1] != '/' && input[i - 1] != '=' && input[i-1] != '!')
 		{
@@ -89,91 +95,75 @@ int numbSize(char* input, int i,bool flag)
 	return numb;
 }
 
-int compute(char* input,size_t size)
+//#Вычесления значений вырожения# 
+//#Принимает: массив преобразованных данных в символы, размер массива#
+int compute(char* input,size_t size)			
 {
 	int result{};
-	{
-		
-		bool flag = true;
-
+	{	
 //processing * and /
-		while (flag)
+		int i{}, a{}, b{};
+		int lnumb{}, rnumb{};
+		while (input[i] != '=')
 		{
-			int a{}, b{}, i{};
-			flag = false;
-			while (input[i] != '=')
+			if (input[i] == '*' || input[i] == '/')
 			{
-				if (input[i] == '*' || input[i] == '/')
+				if (input[i] == '*')
 				{
-					if (input[i] == '*')
-					{
-						int numb1{}, numb2{};
-						numb1 = numbSize(input, i, false);
-						a = charToInt(input, i - 1, numb1);
-						numb2 = numbSize(input, i, true);
-						b = charToInt(input, i + numb2, numb2);
 
-						result = a * b;
-						compArray(input, size, i, result, numb1, numb2);
-						flag = true;
-						break;;
-					}
-					else
-					{
-						int numb1{}, numb2{};
-						numb1 = numbSize(input, i, false);
-						a = charToInt(input, i - 1, numb1);
-						numb2 = numbSize(input, i, true);
-						b = charToInt(input, i + numb2, numb2);
+					lnumb = numbSize(input, i, false);
+					a = charToInt(input, i - 1, lnumb);
+					rnumb = numbSize(input, i, true);
+					b = charToInt(input, i + rnumb, rnumb);
 
-						result = a / b;
-						compArray(input, size, i, result, numb1, numb2);
-						flag = true;
-						break;
-					}
+					result = a * b;
+					compArray(input, size, i, result, lnumb, rnumb);
 				}
-				i++;
+				else
+				{
+					lnumb = numbSize(input, i, false);
+					a = charToInt(input, i - 1, lnumb);
+					rnumb = numbSize(input, i, true);
+					b = charToInt(input, i + rnumb, rnumb);
+
+					result = a / b;
+					compArray(input, size, i, result, lnumb, rnumb);
+				}
+
+				i = 0;		//Снова ищем знак * или /
 			}
+			i++;
 		}
-		flag = true;
 	//Processing + and -
-		while (flag)
+		i = 0;
+		while (input[i] != '=')
 		{
-			int a{}, b{}, i{};
-			flag = false;
-			while (input[i] != '=')
+			if (input[i] == '+' || input[i] == '-')
 			{
-				if (input[i] == '+' || input[i] == '-')
+				if (input[i] == '+')
 				{
-					if (input[i] == '+')
-					{
-						int numb1{}, numb2{};
-						numb1 = numbSize(input, i, false);
-						a = charToInt(input, i - 1, numb1);
-						numb2 = numbSize(input, i, true);
-						b = charToInt(input, i + numb2, numb2);
+					lnumb = numbSize(input, i, false);
+					a = charToInt(input, i - 1, lnumb);
+					rnumb = numbSize(input, i, true);
+					b = charToInt(input, i + rnumb, rnumb);
 
-						result = a + b;
-						compArray(input, size, i, result, numb1, numb2);
-						flag = true;
-						break;
-					}
-					else
-					{
-						int numb1{}, numb2{};
-						numb1 = numbSize(input, i, false);
-						a = charToInt(input, i - 1, numb1);
-						numb2 = numbSize(input, i, true);
-						b = charToInt(input, i + numb2, numb2);
-
-						result = a - b;
-						compArray(input, size, i, result, numb1, numb2);
-						flag = true;
-						break;
-					}
+					result = a + b;
+					compArray(input, size, i, result, lnumb, rnumb);
 				}
-				i++;
+				else
+				{
+					lnumb = numbSize(input, i, false);
+					a = charToInt(input, i - 1, lnumb);
+					rnumb = numbSize(input, i, true);
+					b = charToInt(input, i + rnumb, rnumb);
+
+					result = a - b;
+					compArray(input, size, i, result, lnumb, rnumb);
+				}
+
+				i = 0;			//Снова ищем знак + или -
 			}
+			i++;
 		}
 	}
 	int numb{};
@@ -185,10 +175,9 @@ int compute(char* input,size_t size)
 
 int main()
 {
-	setlocale(0, "");
+	setlocale(0, "ru_RU.utf8");
 	const size_t size = 256;
 	char input[size]{};
-	
 	userInput(input,size);
 	int result{};
 	result = compute(input,size);
